ac03c9985a8bd7e984e2c872e83a7d44be10c006
vidhitapai <80096027+vidhitapai@users.noreply.github.com>
2021-07-28 22:11:07 +0530

    First commit
    First bug solved, one bug fix left

---
+++ Backend Task 1/javascript.js
@@ -0,0 +1,53 @@
+var countObj=
+[
+
+]
+
+var n = prompt("Enter number of words:");
+for(var i = 0; i<Number(n) ; i++)
+{
+    var word = prompt("Enter word");
+    if (i===0)
+    {
+        countObj.push({name:word, value:1});
+    }
+    else
+    {
+        for(var j=0; j<i ; j++)
+        {
+            if (word == countObj[j].name)
+            { 
+                countObj[j].value += 1;
+                break;
+            }
+            else if (j===countObj.length-1)
+            {
+                if(countObj[j] == word)
+                {
+                    countObj[j].value += 1;
+                    break;
+                }
+                countObj[i]={name:" ",value:" "};
+                countObj[i].name=word;
+                countObj[i].value = 1;
+            }
+        }
+    }
+}
+
+var occurences = [];
+var nameArray = [];
+// for(var i =0 ; i<n ; i++)
+// {
+//     nameArray.push(countObj[i].name);
+//     occurences += " " + ((countObj[i].value).toString());
+// }
+
+countObj.forEach((currValue, index, arr) =>
+{
+    nameArray.push(arr[index].name);
+    occurences += (arr[index].value).toString() + " ";
+})
+
+console.log(nameArray.length);
+console.log(occurences);
