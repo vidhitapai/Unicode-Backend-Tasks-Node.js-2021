64d5d2bc957d86ab41aac454aea0f3a8441ab2df
vidhitapai <80096027+vidhitapai@users.noreply.github.com>
2021-08-05 21:22:08 +0530

    Added bonus task!

--- Backend Task 2/javascript2.js
+++ Backend Task 2/javascript2.js
@@ -1,13 +1,21 @@
-var url_bb = 'https://breakingbadapi.com/api/characters'
+var url_bb = 'https://breakingbadapi.com/api/characters?category=Breaking+Bad'
+var url_bcs = 'https://breakingbadapi.com/api/characters?category=Better+Call+Saul'
 
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 
-app.get('/', async (req,res) => {
-    const response = await fetch(url_bb);
-    const data = await response.json();
-    res.send(data);
-})
+app.get('/breakingbad', async (req, res) => {
+    const response1 = await fetch(url_bb);
+    const data1 = await response1.json();
+    res.send(data1);
+});
+
+//bonus task
+app.get('/bettercallsaul', async (req, res) => {
+     const response2 = await fetch(url_bcs);
+     const data2 = await response2.json();
+     res.send(data2);
+});
 
 app.listen(3000);
