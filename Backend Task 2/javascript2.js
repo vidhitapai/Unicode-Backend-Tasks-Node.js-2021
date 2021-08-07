316cf9594fe1638c4a50ce4aea71f81f94d8ce0e
vidhitapai <80096027+vidhitapai@users.noreply.github.com>
2021-08-08 00:46:18 +0530

    Added bonus task + error handling

--- Backend Task 2/javascript2.js
+++ Backend Task 2/javascript2.js
@@ -6,16 +6,37 @@ const app = express();
 const fetch = require('node-fetch');
 
 app.get('/breakingbad', async (req, res) => {
+    try{
+    //fetching the data from api
     const response1 = await fetch(url_bb);
+
+    //parsing data into json format
     const data1 = await response1.json();
+
+    //sending response
     res.send(data1);
+    }
+
+    //error handling
+    catch(error)
+    {
+        console.log(error);
+    }
 });
 
 //bonus task
 app.get('/bettercallsaul', async (req, res) => {
-     const response2 = await fetch(url_bcs);
+    try{
+    const response2 = await fetch(url_bcs);
      const data2 = await response2.json();
      res.send(data2);
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is active!");
+});
